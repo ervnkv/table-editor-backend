2920f31558b2f43ec3eeb674eae44a8aeb654607
ERVNKV <revenkov.egor@gmail.com>
2023-04-18 16:11:32 +0300

    Обработка ошибок

--- src/routes/degree/index.ts
+++ src/routes/degree/index.ts
@@ -7,12 +7,22 @@ export const degreeRouter = () => {
     
     // Получить все уровни образования. GET-запрос
     router.get('/', async (req, res:GetResponse) => {
+        try {
+            
+        } catch (error) {
+            res.sendStatus(500);
+        }
         const degrees = await database.query(`SELECT * FROM degree ORDER BY id`);
         res.json(degrees.rows);
     });
 
     // Добавить уровень образования
     router.post('/', async (req: PostRequest, res: PostResponse) => {
+        try {
+            
+        } catch (error) {
+            res.sendStatus(500);
+        }
         const {name} = req.body;
         const newDegree = await database.query(`INSERT INTO degree (name) values ($1) RETURNING *`, [name]);
         res.json(newDegree.rows[0]);
@@ -20,6 +30,11 @@ export const degreeRouter = () => {
 
     // Изменить уровень образования
     router.put('/', async (req: PutRequest, res:PutResponse) => {
+        try {
+            
+        } catch (error) {
+            res.sendStatus(500);
+        }
         const {id, name} = req.body;
         const newDegree = await database.query(`UPDATE degree set name = $1 where id = $2 RETURNING *`, [name, id]);
         res.json(newDegree.rows[0]);
@@ -27,9 +42,13 @@ export const degreeRouter = () => {
 
     // Удалить уровни образования
     router.delete('/', async (req: DeleteRequest, res: DeleteResponse) => {
-        const ids = (req.body);
-        await database.query(`DELETE FROM degree WHERE id IN (${ids.join()});`);
-        res.json(ids);
+        try {
+            const ids = (req.body);
+            await database.query(`DELETE FROM degree WHERE id IN (${ids.join()});`);
+            res.json(ids); 
+        } catch (error) {
+            res.sendStatus(500);
+        }
     });
 
     return router;
--- src/routes/employee/index.ts
+++ src/routes/employee/index.ts
@@ -4,30 +4,45 @@ import { GetResponse, PostRequest, PostResponse, PutRequest, PutResponse, Delete
 
 export const employeeRouter = () => {
     const router = express.Router();
-    // Запросы к таблице сотрудников "employee". Адрес - "/employee"
 
-    // Получить все уровни образования. GET-запрос
+    // Получить все уровни образования
     router.get('/', async (req, res: GetResponse) => {
-        const employees = await database.query(`SELECT * FROM employee ORDER BY id`);
-        res.json(employees.rows);
+        try {
+            const employees = await database.query(`SELECT * FROM employee ORDER BY id`);
+            res.json(employees.rows);
+        } catch (error) {
+            res.sendStatus(500);
+        }
     });
-    // Добавить сотрудника. POST-запрос, в теле json в виде {"name": "Сотрудник 1", "degree_id": "1"}
+    // Добавить сотрудника
     router.post('/', async (req: PostRequest, res: PostResponse) => {
-        const {name, degree_id} = req.body;
-        const newEmployee = await database.query(`INSERT INTO employee (name, degree_id) values ($1, $2) RETURNING *`, [name, degree_id]);
-        res.json(newEmployee.rows[0]);
+        try {
+            const {name, degree_id} = req.body;
+            const newEmployee = await database.query(`INSERT INTO employee (name, degree_id) values ($1, $2) RETURNING *`, [name, degree_id]);
+            res.json(newEmployee.rows[0]);
+        } catch (error) {
+            res.sendStatus(500);
+        }
     });
-    // Изменить сотрудника. POST-запрос, в теле json в виде {"id": "1", "name": "Сотрудник 1", "degree_id": "1"}
+    // Изменить сотрудника
     router.put('/', async (req: PutRequest, res: PutResponse) => {
-        const {id, name, degree_id} = req.body;
-        const newEmployee = await database.query(`UPDATE employee set name = $1, degree_id = $2 where id = $3 RETURNING *`, [name, degree_id, id]);
-        res.json(newEmployee.rows[0]);
+        try {
+            const {id, name, degree_id} = req.body;
+            const newEmployee = await database.query(`UPDATE employee set name = $1, degree_id = $2 where id = $3 RETURNING *`, [name, degree_id, id]);
+            res.json(newEmployee.rows[0]);
+        } catch (error) {
+            res.sendStatus(500);
+        }
     });
-    // Удалить сотрудников. DELETE-запрос, в теле json в виде "ids": "[1,2]"
+    // Удалить сотрудников
     router.delete('/', async (req: DeleteRequest, res: DeleteResponse) => {
-        const ids = (req.body)
-        await database.query(`DELETE FROM employee WHERE id IN (${ids.join()});`)
-        res.json(ids);
+        try {
+            const ids = (req.body)
+            await database.query(`DELETE FROM employee WHERE id IN (${ids.join()});`)
+            res.json(ids);
+        } catch (error) {
+            res.sendStatus(500);
+        }
     });
 
     return router;
